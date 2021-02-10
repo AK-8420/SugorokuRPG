ea4777d31a9f05239b977a97ffdde716563b457c
AK-8420 <katainu0103@gmail.com>
2021-02-11 03:18:02 +0900

    Graphicsクラスの完成

--- SugorokuRPG_2P/Graphic.h
+++ SugorokuRPG_2P/Graphic.h
@@ -6,6 +6,11 @@ private:
 	int width, height;
 	int numSliceX, numSliceY;
 	int* handle;
+public:
+	int x = 0, y = 0;
+	int index = 0;
+	double exRate = 1; //拡大率
+	double Angle = 0; //回転率
 
 	Graphics(const TCHAR* path, int w, int h, int sx, int sy) {
 		width = w;
@@ -22,5 +27,16 @@ private:
 		else {
 			LoadDivGraph(path, num, numSliceX, numSliceY, width / numSliceX, height / numSliceY, handle);
 		}
+	}
+	void draw() {
+		DrawRotaGraph(x, y, exRate, Angle, handle[index], 1);
+	}
+	// インデックスを指定して描画
+	void draw(int i) {
+		DrawRotaGraph(x, y, exRate, Angle, handle[i], 1);
+	}
+	void setPos(int X, int Y) {
+		x = X;
+		y = Y;
 	}
 };
